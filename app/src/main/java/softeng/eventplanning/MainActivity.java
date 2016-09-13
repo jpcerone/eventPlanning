0f377f8d05b95ebf1ac21c0437148bcba3beb72f
jawolf17 <jawolf@buffalo.edu>
2016-09-12 23:41:49 -0400

    Basic Layout for Event Creation

--- app/src/main/java/softeng/eventplanning/MainActivity.java
+++ app/src/main/java/softeng/eventplanning/MainActivity.java
@@ -2,12 +2,29 @@ package softeng.eventplanning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.create_event);
+    }
+
+    public void setLocation(View v){
+        setContentView(R.layout.create_event_location);
+    }
+
+    public void setDate(View v){
+        setContentView(R.layout.create_event_date);
+    }
+
+    public void setTime(View v){
+        setContentView(R.layout.create_event_time);
+    }
+
+    public void done(View v){
+        setContentView(R.layout.create_event);
     }
 }
