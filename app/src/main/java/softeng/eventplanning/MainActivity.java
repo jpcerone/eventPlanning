66864289150c7709961772a35033d654a5d4baa7
jpcerone <jpcerone@gmail.com>
2016-09-15 14:14:29 -0400

    Made minute changes

--- app/src/main/java/softeng/eventplanning/MainActivity.java
+++ app/src/main/java/softeng/eventplanning/MainActivity.java
@@ -3,9 +3,12 @@ package softeng.eventplanning;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         Intent intent = new Intent(this, Search.class);
         startActivity(intent);
--- app/src/main/java/softeng/eventplanning/sFilters.java
+++ app/src/main/java/softeng/eventplanning/sFilters.java
@@ -5,11 +5,20 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
 public class sFilters extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.searchfilter);
+    }
+    public void filterBack(View view)
+    {
+        Intent intent = new Intent(this, Search.class);
+        startActivity(intent);
     }
 
 
