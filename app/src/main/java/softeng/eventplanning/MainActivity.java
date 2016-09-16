76118ec798d10c447a5820aee4b3e6f1776b6a87
daw24 <daw24@buffalo.edu>
2016-09-15 20:42:05 -0400

    Added example of scrollable wall with 4 sample events TODO: Settings button, map swipe up example

--- app/src/main/java/softeng/eventplanning/MainActivity.java
+++ app/src/main/java/softeng/eventplanning/MainActivity.java
@@ -2,12 +2,28 @@ package softeng.eventplanning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.wall_page);
     }
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void settingsMenu(View v) {
+        setContentView(R.menu.settings_menu);
+    }
+    */
 }
