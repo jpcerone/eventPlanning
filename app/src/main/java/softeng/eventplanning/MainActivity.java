af8576ef4d489ffc3d4b841a14c962a27c7856c7
daw24 <daw24@buffalo.edu>
2016-09-18 14:26:59 -0400

    Add setting/logout menu button

--- app/src/main/java/softeng/eventplanning/MainActivity.java
+++ app/src/main/java/softeng/eventplanning/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,16 +15,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wall_page);
     }
-/*
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.settings_menu, menu);
         return true;
     }
 
-    public void settingsMenu(View v) {
-        setContentView(R.menu.settings_menu);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (id) {
+            case R.id.settings_notifications:
+                // TODO go to notifications page
+                Toast.makeText(getApplicationContext(),
+                        "Settings Updated",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.settings_themes:
+                // TODO go to theme page
+                Toast.makeText(getApplicationContext(),
+                        "Settings Updated",
+                        Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
-    */
+
+    public void eventClicked(View view){
+        // TODO open event's page when clicked
+    }
+
+    public void settingsClicked(View view){
+        // TODO open settings view when clicked
+    }
 }
