65e732e989e58a686a6f4e7402a38d6e4ae28aa8
jpcerone <jpcerone@gmail.com>
2016-09-18 12:13:44 -0400

    Fixed Push

--- app/src/main/java/softeng/eventplanning/MainActivity.java
+++ app/src/main/java/softeng/eventplanning/MainActivity.java
@@ -2,6 +2,7 @@ package softeng.eventplanning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost tab = (TabHost) findViewById(R.id.mainTabs);
+        tab.setup();
+        TabHost.TabSpec spec1 = tab.newTabSpec("TAB 1");
+        spec1.setIndicator("TAB 1");
+        spec1.setContent(R.id.layout1);
+        tab.addTab(spec1);
+
+        TabHost.TabSpec spec2 = tab.newTabSpec("TAB 2");
+        spec2.setIndicator("TAB 2");
+        spec2.setContent(R.id.layout2);
+        tab.addTab(spec2);
+
     }
 }
