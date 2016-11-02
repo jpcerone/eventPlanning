acd634aeedb4f21e098269dd4773cf33c5b16694
jpcerone <jpcerone@gmail.com>
2016-11-02 18:36:25 -0400

    Account menu option added, account settings page created

---
+++ app/src/main/java/softeng/eventplanning/SettingsPage.java
@@ -0,0 +1,19 @@
+package softeng.eventplanning;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by josh on 11/2/16.
+ */
+public class SettingsPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_page);
+
+
+    }
+
+}
