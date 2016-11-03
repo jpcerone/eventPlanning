9f4f73584111c3a0ee713ecf7cf933c3dbb90d91
daw24 <daw24@buffalo.edu>
2016-11-03 13:31:58 -0400

    Added wallAdapter and Wallitem Classes back in, TODO populate array being grabbed

---
+++ app/src/main/java/API/FeedAPI.java
@@ -0,0 +1,8 @@
+package API;
+
+/**
+ * Created by David on 10/31/2016.
+ */
+
+public class FeedAPI {
+}
---
+++ app/src/main/java/softeng/eventplanning/WallAdapter.java
@@ -0,0 +1,8 @@
+package softeng.eventplanning;
+
+/**
+ * Created by David on 11/2/2016.
+ */
+
+public class WallAdapter {
+}
