c5dca22d6ffa83950aa12839abd4c3e71be10d42
jpcerone <jpcerone@gmail.com>
2016-11-04 13:06:31 -0400

    Real commit

--- app/src/main/java/softeng/eventplanning/userPage.java
+++ app/src/main/java/softeng/eventplanning/userPage.java
@@ -1,8 +1,43 @@
 package softeng.eventplanning;
 
-/**
- * Created by jpcerone on 11/4/16.
- */
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class userPage {
+import API.SettingsAPI;
+
+
+public class userPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_page);
+
+
+
+
+    }
+    public void updateUserPage(String[] info){
+        TextView editTextPlace;
+
+        editTextPlace = (TextView) findViewById(R.id.user_bday);
+        editTextPlace.setText(info[0]);
+        editTextPlace = (TextView) findViewById(R.id.user_fname);
+        editTextPlace.setText(info[1]);
+        editTextPlace = (TextView) findViewById(R.id.user_lname);
+        editTextPlace.setText(info[2]);
+        editTextPlace = (TextView) findViewById(R.id.user_bio);
+        editTextPlace.setText(info[3]);
+        editTextPlace = (TextView) findViewById(R.id.user_phone);
+        editTextPlace.setText(info[4]);
+        editTextPlace = (TextView) findViewById(R.id.user_username);
+        editTextPlace.setText(info[5]);
+    }
+
+
 }
